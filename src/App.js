656bdda6711e261983ebf5f3e917019e76923733
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2022-11-03 12:24:44 +0300

    Add correct link

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to Our Capstone Project</p>
+        <p>Not Completed yet</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.google.com/document/d/1cTqE6s6ox1Em3Ujkdpvze_bv1VkwV11IOsfF8G3mQJo/edit?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
